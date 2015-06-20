a501b24b24d38293a150b95f4f2206a911cec630
Sheila Koomson <sheilababbey@yahoo.com>
2015-06-20 23:04:18 +0100

    Indentation fixed. Closed #24

--- greet/Greet.java
+++ greet/Greet.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
+
 public class Greet{
 	public static void main(String[] args){
 		System.out.println("Hi there. What is your name? ");
-			Scanner scanner = new Scanner(System.in);
-				String name = scanner.nextLine();
-					if (name.equals("")){
-						System.out.println("Coward!");
-					}
-							else{
-							System.out.println("oh hello," + name + ",welcome to java!");
-							}
-						
-	}
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        if (name.equals("")){
+            System.out.println("Coward!");
+        }
+        else{
+            System.out.println("oh hello," + name + ",welcome to java!");
+        }
+        
+    }
 }
