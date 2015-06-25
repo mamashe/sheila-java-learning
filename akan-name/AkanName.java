3a4baf6db9326d8aa49e4db56035ff448d10f043
Sheila Koomson <sheilababbey@yahoo.com>
2015-06-25 21:09:49 +0100

    Akan name App. Ref #16.

--- akan-name/AkanName.java
+++ akan-name/AkanName.java
@@ -1,95 +1,52 @@
-import java.util.Scanner;
+import java.io.Console;
 
 public class AkanName{
-    public String gender;
-    public String days;
     
-    public AkanName(){
-        gender = ("Male, Famle");
-        days = ("Monday, Tuesday, Wednessday, Thursday, Friday");
-        
-    }
-    
-    public String getGender(){
-        Scanner scanner =new Scanner(System.in);
+    public static int readDayOfWeek(){
+        String[] weekDays = {"Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday", "Sunday"};
+        Console console = System.console();
         while(true){
-          
-            String gen = scanner.nextLine();
-            if(!gen.equals("gender")){
-                System.out.printf("%s is not a gender type. Type a correct gender. ", gen);
-                continue;
+            String input = console.readLine("Enter A Day Of The Week: ");
+            for(int i = 0; i<weekDays.length; ++i){
+                if(input.equals (weekDays[i])){
+                    return i;
+                    
+                }
+                
             }
-            return gen;
-        } 
+            System.out.println("Sorry you have typed invalid day. Try again.");
+        }
     }
     
-    public String getDay(){
-        Scanner scanner =new Scanner(System.in);
+    public static int readGender(){
+        Console console = System.console();
         while(true){
-            System.out.println("Please Enter The Day You Were Born: ");
-            String day1 = scanner.nextLine();
-            if(!day1.equals("days")){
-                System.out.printf("%s is not a day. Type a correct day. ", day1);
-                continue;
+            String input = console.readLine("Enter Your Gender: ");
+            if(input.equals("male")){
+                return 0;
             }
-            return day1;
-        }   
-    }
-    
-    public String getDay1(String mal){
-        if(mal.equals("Monday")){
-            System.out.println("Your Name is Kojo");
+            else if(input.equals("female")){
+                return 1;
+            }
+            else{
+                System.out.println("Sorry wrong gender type");
+            }
         }
-        else if(mal.equals("Tuesday")){
-			System.out.println("Your Name is Kobina");
-        }
-        else if(mal.equals("Wednessday")){
-            System.out.println("Your Name is Kwaku");
-        }
-        else if(mal.equals("Thursday")){
-			System.out.println("Your Name is Yaw");
-        }
-        else if(mal.equals("Friday")){
-			System.out.println("Your Name is Kofi");
-        }
-        else if(mal.equals("Saturday")){
-			System.out.println("Your Name is Kwame");
-        }
-        else if(mal.equals("Sunday")){
-			System.out.println("Your Name is Kwasi");
-        }
-        return mal;
-    }
-    
-    
-    
-    
-    public String getFemale(String fe){
-        if(fe.equals("Monday")){
-            System.out.println("Your Name is Adjoa");
-        }
-        else if(fe.equals("Tuesday")){
-			System.out.println("Your Name is Abina");
-        }
-        else if(fe.equals("Wednessday")){
-			System.out.println("Your Name is Akua");
-        }
-        else if(fe.equals("Thursday")){
-			System.out.println("Your Name is Yaa");
-        }
-        else if(fe.equals("Friday")){
-			System.out.println("Your Name is Afua");
-        }
-        else if(fe.equals("Saturday")){
-			System.out.println("Your Name is Ama");
-        }
-        else if(fe.equals("Sunday")){
-			System.out.println("Your Name is Akosua");
-        }
-        return fe;
     }
     
     public static void main(String[] args){
-          System.out.println("Please Enter Your Gender: "); 
+       String[] femaleNames = {"Adwoa", "Abena", "Ekua", "Yaa", "Efua", "Ama", "Akosua"};
+       String[] maleNames = {"Kwojo", "Kwabena", "Kweku", "Yaw", "Kofi", "Kwame", "Kwesi"};
+      //String[] names = {"Adwoa", "Abena", "Ekua", "Yaa", "Efua", "Ama", "Akosua", "Kwojo", "Kwabena", "Kweku", "Yaw", "Kofi", "Kwame", "Kwesi"};
+        int gender = readGender();
+        int dayIndex = readDayOfWeek();
+        
+        //System.out.println("I got: " + gender);
+        if(gender == 0){
+            System.out.println("That Corresponds to: " + maleNames[dayIndex]);
+        }
+        else{
+            System.out.println("That Corresponds to: " + femaleNames[dayIndex]);
+        }
     }
 }
