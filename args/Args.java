63765c174ea032d7781ddc4e76c39e323ab5f330
Sheila Koomson <sheilababbey@yahoo.com>
2015-07-01 21:49:54 +0100

    Command-line Arg. Closed #21.

--- args/Args.java
+++ args/Args.java
@@ -1,6 +1,7 @@
 public class Args{
 	public static void main(String[] args){
 		int i = 1;
+            System.out.printf("Got %d command-line arguments.\n", args.length);
 			for (String arg : args) {
 				System.out.printf("%3d. %s\n", i, arg);
 					++i;
