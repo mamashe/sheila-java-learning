3a54ecb2b1de9d3e13440a9d1831984f6d30231e
Sheila Koomson <sheilababbey@yahoo.com>
2015-07-01 21:54:07 +0100

    Indentation. Closed #22.

--- args/Args.java
+++ args/Args.java
@@ -1,10 +1,10 @@
 public class Args{
 	public static void main(String[] args){
 		int i = 1;
-            System.out.printf("Got %d command-line arguments.\n", args.length);
-			for (String arg : args) {
-				System.out.printf("%3d. %s\n", i, arg);
-					++i;
-			}
-	}
+        System.out.printf("Got %d command-line arguments.\n", args.length);
+        for (String arg : args) {
+            System.out.printf("%3d. %s\n", i, arg);
+            ++i;
+        }
+    }
 }
