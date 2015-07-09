29b8ed7d2089ba3860718b81a46fef87fe8dcefb
Sheila Koomson <sheilababbey@yahoo.com>
2015-07-09 22:44:19 +0100

    Triangle. Closed #25.

--- triangle/Triangle.java
+++ triangle/Triangle.java
@@ -5,9 +5,9 @@ public class Triangle{
         Scanner scanner = new Scanner(System.in);
         double base, height, ans;
         System.out.println("What is the base of a triangle? ");
-        base = scanner.nextInt();
+        base = scanner.nextDouble();
         System.out.println("What is the height of a triangle? ");
-        height = scanner.nextInt();
+        height = scanner.nextDouble();
         ans = base * height/2;
         System.out.printf("The Area Of A Triangle is %2f. ", ans);
         
