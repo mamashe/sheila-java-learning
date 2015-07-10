4fa9f89fd15c74bd9f118ff3aec1c7983b6c9e72
Sheila Koomson <sheilababbey@yahoo.com>
2015-07-10 16:48:21 +0100

    toString() method. Closed #30.

---
+++ tostring-test/App.java
@@ -0,0 +1,10 @@
+public class App{
+    public static void main(String[] args){
+        Entity entity = new Entity();
+        System.out.println(entity);
+        
+        Person p = new Person();
+        p.setName("Sheila");
+        System.out.println(p);
+    }
+}
---
+++ tostring-test/Entity.java
@@ -0,0 +1,5 @@
+public class Entity{
+    public String toString(){
+       return "Yipee! I am an Entity object ";
+    }
+}
