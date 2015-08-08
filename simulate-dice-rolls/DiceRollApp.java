7ce09ceb135eb7de6fe384d6f517818105970c9d
Sheila Koomson <sheilababbey@yahoo.com>
2015-08-08 10:00:08 +0100

    Simulate Dice Rolls. Closed #42.

---
+++ simulate-dice-rolls/DiceRollApp.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.HashMap;
+
+public class DiceRollApp{
+    public static void main(String[] args){
+        System.out.println("Enter the number of times you want to roll the dice: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        
+        Random random = new Random();
+        
+        HashMap<Integer, Integer> dice = new HashMap<Integer, Integer>();
+        dice.put(1, 0);
+        dice.put(2, 0);
+        dice.put(3, 0);
+        dice.put(4, 0);
+        dice.put(5, 0);
+        dice.put(6, 0);
+        
+        for(int i = 0; i <= input; i++){
+            int roll = random.nextInt(6)+1;
+            dice.replace(roll, dice.get(roll) +1);
+        
+        }
+        
+        for(Integer k: dice.keySet()){
+        System.out.printf("%d: %d\n ", k, dice.get(k));
+        }
+    }
+}
