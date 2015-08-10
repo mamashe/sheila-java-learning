b6a823901ba027bcbaec605bf5c9be31d8741973
Sheila Koomson <sheilababbey@yahoo.com>
2015-08-10 11:06:48 +0100

    Random range 5 - 50. Ref # 41.

---
+++ randrange/RandomRange.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class RandomRange{
+    public static void main(String[] args){
+        int minValue = 5;
+        int maxValue = 50;
+        Random random = new Random();
+        for(int i = 0; i <= 10000; i++){
+            int range = random.nextInt(11)+5;
+            if(range <= ){
+                minValue++;
+            }
+           // else if(range >= maxValue){
+                //maxValue++;
+            //}
+            else{
+             maxValue++;
+            }
+           
+        
+        
+        }
+        System.out.printf("Minmum value is %d\n and MaxValue is %d", maxValue, minValue);
+    }
+}
